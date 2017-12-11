b76279b6267caa047470dba50899e52b48b4e583
Meraz Haque <mhaque@pacbrands.com.au>
2017-12-12 09:51:07 +1100

    Lumen working

--- src/vendor/composer/autoload_real.php
+++ src/vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit79d1390aacce3aef56af953defb981c4
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit79d1390aacce3aef56af953defb981c4::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire79d1390aacce3aef56af953defb981c4($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire79d1390aacce3aef56af953defb981c4($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
